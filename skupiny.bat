e9e937b3052e1ac0648a6e25b6057ae1aeae751e
Folki23 <128801741+Folki23@users.noreply.github.com>
2024-01-30 09:56:54 +0100

    Update skupiny.bat

--- skupiny.bat
+++ skupiny.bat
@@ -1,7 +1,9 @@
 @echo off
 
 for /f "tokens=1-4 delims=," %%a in (uziv.txt) do (
-    net user "%%a" "%%b" /add /fullname: "%%a" & net localgroup "%%c" "%%a" /add
+    net user "%%a" "%%b" /add 
+    net localgroup "%%c" /add
+    net localgroup "%%c" "%%a" /add
     echo Jmeno: "%%a" Heslo: "%%b" skupina: "%%c"
 
     
